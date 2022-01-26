1437aed9f2a09a83fbed842f8df38af69d333cca
Martin Kock <code@deeagle.de>
2022-01-26 15:57:48 +0100

    change: first impl tasks to create users via api.

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,6 +1,10 @@
 package users
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"myapp/domain/users"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,5 +15,16 @@ func GetUser(c *gin.Context) {
 }
 
 func CreateUser(c *gin.Context) {
+	var user users.User
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	json_err := json.Unmarshal(bytes, &user)
+	if json_err != nil {
+		log.Println(json_err)
+		return
+	}
 	c.String(http.StatusNotImplemented, "not implemented yet")
 }
