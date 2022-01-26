65f59d5c134ceb01e1b3e7ff4085b9a2cf7fdf87
Martin Kock <code@deeagle.de>
2022-01-26 18:05:52 +0100

    change: implement basic user GET.

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -6,12 +6,25 @@ import (
 	"myapp/services"
 	"myapp/utils/errors"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "not implemented yet")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+	}
+
+	c.JSON(http.StatusOK, user)
 }
 
 func CreateUser(c *gin.Context) {
--- services/users_service.go
+++ services/users_service.go
@@ -5,6 +5,16 @@ import (
 	"myapp/utils/errors"
 )
 
+func GetUser(userId int64) (*users.User, *errors.RestErr) {
+	result := &users.User{Id: userId}
+	err := result.Get()
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
 func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 	userErr := user.Validate()
 	if userErr != nil {
