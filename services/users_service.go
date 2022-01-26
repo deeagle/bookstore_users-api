764847a0a5ecbce007018f2a9e7def0709bdf0d8
Martin Kock <code@deeagle.de>
2022-01-26 17:23:26 +0100

    change: validate user email in service.

--- services/users_service.go
+++ services/users_service.go
@@ -3,8 +3,13 @@ package services
 import (
 	"myapp/domain/users"
 	"myapp/utils/errors"
+	"strings"
 )
 
 func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	user.Email = strings.TrimSpace(strings.ToLower(user.Email))
+	if user.Email == "" {
+		return nil, errors.NewBadRequestError("invalid email address")
+	}
 	return &user, nil
 }
